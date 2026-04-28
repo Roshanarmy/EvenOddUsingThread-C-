409e87145d02dccc5a03c573df34597e84922dfb
Roshan Srivastava <58077642+Roshanarmy@users.noreply.github.com>
2026-04-28 21:13:38 +0530

    Create Program.cs
    Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+
+class Program
+{
+    public static void Main()
+    {
+       
+
+        Console.WriteLine("output Even Odd Using Multithreading :-");
+        EvenOddUsingThread.Run2();
+    }
+}
+
